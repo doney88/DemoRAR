d57ad8cfd3f0baaaf08202aeb6b85b51e4aa4427
doney88 <cd_doney@hotmail.com>
2020-04-14 22:30:51 +0800

    Clean Code

--- DemoRAR/RARHelper.cs
+++ DemoRAR/RARHelper.cs
@@ -13,6 +13,43 @@ namespace DemoRAR
 {
     public class RARHelper
     {
+        static string WinRARRegeditPath= @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe";
+        #region 私有方法
+        /// <summary>
+        /// 获取WinRAR应用程序路径，如果注册表中没有则返回当前目录下\res\WinRAR.exe
+        /// </summary>
+        /// <returns>返回注册表中指定的RAR应用程序路径，没有则返回当前目录下\res\WinRAR.exe</returns>
+        private string GetExePath()
+        {
+            RegistryKey the_Reg = Registry.LocalMachine.OpenSubKey(WinRARRegeditPath);//访问储存应用程序注册表信息键值
+            string exePath = the_Reg.GetValue("").ToString();//查询WINRAR注册表储存路径
+            the_Reg.Close();
+            if (string.IsNullOrEmpty(exePath))    //如果存在则表示应用程序存在于本计算机中
+                exePath = Application.StartupPath + "\\res\\WinRAR.exe";
+            return exePath;
+        }
+        /// <summary>
+        /// 创建控制WinRAR的Process类，并设置好ProcessStartInfo属性
+        /// </summary>
+        /// <param name="workDirectory">压缩包的路径</param>
+        /// <param name="rarCmd">命令符</param>
+        /// <param name="exePath"></param>
+        /// <returns></returns>
+        private Process CreateRARProcess(string workDirectory, string rarCmd, string exePath,bool useShellExecute = false)
+        {
+            ProcessStartInfo startinfo = new ProcessStartInfo();
+            startinfo.FileName = exePath; //应用程序路径
+            startinfo.Arguments = rarCmd; //设置命令参数  
+            startinfo.WindowStyle = ProcessWindowStyle.Hidden; //隐藏 WinRAR 窗口  
+            startinfo.WorkingDirectory = workDirectory;
+            startinfo.UseShellExecute = useShellExecute;
+            Process process = new Process();
+            process.StartInfo = startinfo;
+            return process;
+        }
+
+        #endregion
+
         /// <summary>
         /// 压缩文件
         /// </summary>
@@ -20,33 +57,18 @@ namespace DemoRAR
         /// <param name="rarFolder">压缩文件目录</param>
         /// <param name="rarName">压缩文件名</param>
         /// <returns></returns>
-        public static bool RAR(string sourceFolder, string rarFolder, string rarName)
+        public bool RAR(string sourceFolder, string rarFolder, string rarName)
         {
-            bool flag = false;
-            string winRARexePath;    //WinRAR.exe 的完整路径  
-            RegistryKey regKey; //注册表键  
-            Object regValue;   //键值  
-            string cmd;     //WinRAR 命令参数  
-            ProcessStartInfo startinfo;
+            string rarCmd;     //WinRAR 命令参数  
+            bool flag = false ;
             Process process;
             try
             {
-                regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe");
-                regValue = regKey.GetValue(""); // 键值为 "d:\Program Files\WinRAR\WinRAR.exe" "%1"  
-                winRARexePath = regValue.ToString();
-                regKey.Close();
-                //rarexe = rarexe.Substring(1, rarexe.Length - 7); // d:\Program Files\WinRAR\WinRAR.exe  
+                string exePath = GetExePath();
                 Directory.CreateDirectory(rarFolder);
                 //压缩命令，相当于在要压缩的文件夹(path)上点右键 ->WinRAR->添加到压缩文件->输入压缩文件名(rarName)  
-                cmd = string.Format("a {0} {1} -r -ep1",  rarName, sourceFolder);
-                startinfo = new ProcessStartInfo();
-                if (!File.Exists(winRARexePath)) winRARexePath = Application.StartupPath + "\\res\\WinRAR.exe";
-                startinfo.FileName = winRARexePath;
-                startinfo.Arguments = cmd; //设置命令参数  
-                startinfo.WindowStyle = ProcessWindowStyle.Hidden; //隐藏 WinRAR 窗口  
-                startinfo.WorkingDirectory = rarFolder;
-                process = new Process();
-                process.StartInfo = startinfo;
+                rarCmd = string.Format("a {0} {1} -r -ep1", rarName, sourceFolder); //a 表示压缩 -r 表示囊括子文件夹 -ep1表示不要囊括父文件夹
+                process = CreateRARProcess(rarFolder, rarCmd, exePath);
                 process.Start();
                 process.WaitForExit(); //无限期等待进程 winrar.exe 退出  
                 if (process.HasExited)
@@ -61,7 +83,6 @@ namespace DemoRAR
             }
             return flag;
         }
-
         /// <summary>
         /// 解压缩
         /// </summary>
@@ -69,32 +90,18 @@ namespace DemoRAR
         /// <param name="rarFolder">RAR压缩包存放路径</param>
         /// <param name="rarName">RAR压缩包名字</param>
         /// <returns></returns>
-        public static bool UnRAR(string fileFolder, string rarFolder,string rarName)
+        public bool UnRAR(string fileFolder, string rarFolder,string rarName)
         {
             bool flag = false;
-            string rarexePath;
-            RegistryKey regKey;
-            Object regValue;
             string rarCmd;
-            ProcessStartInfo startinfo;
             Process process;
             try
             {
-                regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe");
-                regValue = regKey.GetValue("");
-                rarexePath = regValue.ToString();
-                regKey.Close();
+                string exePath = GetExePath();
                 Directory.CreateDirectory(fileFolder);
                 //解压缩命令，相当于在要压缩文件(rarName)上点右键 ->WinRAR->解压到当前文件夹  -y表示如果存在相同文件则覆盖
-                rarCmd = string.Format("x {0} {1} -y", rarFolder + "\\" +rarName, fileFolder);
-                startinfo = new ProcessStartInfo();
-                if (!File.Exists(rarexePath)) rarexePath = Application.StartupPath + "\\res\\WinRAR.exe";
-                startinfo.FileName = rarexePath;
-                startinfo.Arguments = rarCmd;
-                startinfo.WindowStyle = ProcessWindowStyle.Hidden;
-                startinfo.WorkingDirectory = rarFolder;
-                process = new Process();
-                process.StartInfo = startinfo;
+                rarCmd = string.Format("x {0} {1} -y", rarFolder + "\\" + rarName, fileFolder);
+                process = CreateRARProcess(rarFolder, rarCmd, exePath);
                 process.Start();
                 process.WaitForExit();
                 if (process.HasExited)
@@ -116,40 +123,24 @@ namespace DemoRAR
         /// <param name="rarFolder">RAR压缩包存放路径</param>
         /// <param name="rarName">RAR压缩包名字</param>
         /// <returns></returns>
-        public static Process UnRARAsync(string fileFolder, string rarFolder, string rarName)
+        public Process UnRARAsync(string fileFolder, string rarFolder, string rarName)
         {
-            string rarexe;
-            RegistryKey regkey;
-            Object regvalue;
-            string cmd;
-            ProcessStartInfo startinfo;
+            string rarCmd;
             Process process;
             try
             {
-                regkey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe");
-                regvalue = regkey.GetValue("");
-                rarexe = regvalue.ToString();
-                regkey.Close();
+                string exePath = GetExePath();
+                Directory.CreateDirectory(fileFolder);
+                //解压缩命令，相当于在要压缩文件(rarName)上点右键 ->WinRAR->解压到当前文件夹  
+                rarCmd = string.Format(" x {0} {1} -y", rarFolder + "\\" + rarName, fileFolder);
+                process = CreateRARProcess(rarFolder, rarCmd, exePath,true);
+                process.Start();
+                return process;
             }
-            catch
+            catch (Exception e)
             {
-                //throw e;
-                rarexe = Application.StartupPath + "\\WinRAR.exe";
+                throw e;
             }
-            Directory.CreateDirectory(fileFolder);
-            //解压缩命令，相当于在要压缩文件(rarName)上点右键 ->WinRAR->解压到当前文件夹  
-            cmd = string.Format(" x {0} {1} -y", rarFolder + "\\" + rarName, fileFolder );
-            startinfo = new ProcessStartInfo();
-            if (!File.Exists(rarexe)) rarexe = Application.StartupPath + "\\res\\WinRAR.exe";
-            startinfo.FileName = rarexe;
-            startinfo.Arguments = cmd;
-            startinfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startinfo.WorkingDirectory = rarFolder;
-            startinfo.UseShellExecute = true;
-            process = new Process();
-            process.StartInfo = startinfo;
-            process.Start();
-            return process;
         }
     }
 }
